ed48fcc95383b7e42855c6d4e6a4a62fd027f1c2
irzinfante <irzinfante@gmail.com>
2022-07-20 22:59:27 +0200

    Mint token to creator

--- hardhat/contracts/token/EVMtoken.sol
+++ hardhat/contracts/token/EVMtoken.sol
@@ -8,10 +8,8 @@ import "../openzeppelin/token/ERC20/ERC20.sol";
 // Total Supply           : 10M --> 10000000 * 10**18
 // Decimals               : 18
 
-// Provisioning address (testnet) : 0xd331C36e919f773f0D9B5cAbff5F80C0793a3DB8
-
 contract EVMtoken is ERC20 {
     constructor() ERC20("A token for interoperability", "EVM") {
-        _mint(0xd331C36e919f773f0D9B5cAbff5F80C0793a3DB8, 10000000 * 10**uint(decimals()));
+        _mint(msg.sender, 10000000 * 10**uint(decimals()));
     }
 }
